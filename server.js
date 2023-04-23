38f75f24eeb18bbb1d4596922d0a962b5abe6cec
jmcavaddy <jakemcavaddy@gmail.com>
2023-04-23 00:53:24 -0400

    Trying to fix heroku error by changing port

--- server.js
+++ server.js
@@ -78,6 +78,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(process.env.PORT || 3001, () =>
+app.listen(process.env.PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
